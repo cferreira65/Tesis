ecf29866ce9e3a84bdad295cba6d27227910aa42
URIJI JAMI <uriji@URIJIs-MacBook.local>
2017-08-28 19:34:26 -0400

    archivos de busqueda para cada aplicacion

---
+++ get_users4.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+import sys
+import string
+import simplejson
+import pandas as pd
+import tweepy
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+import botometer
+
+# All the files containing the tweets
+df1 = pd.read_csv('Tweets/sosvenezuela_17-01.csv', sep= ';')
+df2 = pd.read_csv('Tweets/sosvenezuela_17-02.csv', sep= ';')
+df3 = pd.read_csv('Tweets/sosvenezuela_17-03.csv', sep= ';')
+df4 = pd.read_csv('Tweets/sosvenezuela_17-04.csv', sep= ';')
+df5 = pd.read_csv('Tweets/sosvenezuela_17-05.csv', sep= ';')
+df6 = pd.read_csv('Tweets/ChavezVive15-04-17.csv', sep= ';')
+df7 = pd.read_csv('Tweets/ChavezVive15-08-13.csv', sep= ';')
+df8 = pd.read_csv('Tweets/ChavezVive15-08-14.csv', sep= ';')
+df9 = pd.read_csv('Tweets/ChavezVive15-08-15.csv', sep= ';')
+df10 = pd.read_csv('Tweets/ChavezVive15-08-16.csv', sep= ';')
+df11 = pd.read_csv('Tweets/selected_users.csv', sep =';')
+
+# Getting the username of the tweets
+ids = df1.username 
+ids2 = df2.username
+ids3 = df3.username
+ids4 = df4.username
+ids5 = df5.username
+ids6 = df6.username
+ids7 = df7.username
+ids8 = df8.username
+ids9 = df9.username
+ids10 = df10.username
+ids11 = df11.username
+
+ids = ids.append(ids2)
+ids = ids.append(ids3)
+ids = ids.append(ids4)
+ids = ids.append(ids5)
+ids = ids.append(ids6)
+ids = ids.append(ids7)
+ids = ids.append(ids8)
+ids = ids.append(ids9)
+ids = ids.append(ids10)
+#ids = ids.append(ids11)
+
+ids = pd.Series(ids.unique())
+print(ids.size)
+
+
+CONSUMER_KEY = 't1qq29K8Ua6cw4JL8Oheyf1Ky'
+CONSUMER_SECRET = '7WVCKC32BlfXNIKrPTbCy7Vp0BRDX4isTD9w7Zrxsjgdx8QRni'
+ACCESS_KEY = '86460420-eLo5G87QXcQOKc7KxyzVuDOwK1sEVNlhHMsrtgaX2'
+ACCESS_SECRET = 'e5dK84Sxmt69lLam2B5kUnWBjt3hkQRP1JehKGapGZGYj'
+auth = OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+auth.secure = True
+api = tweepy.API(auth)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+class TweetListener(StreamListener):
+    # A listener handles tweets are the received from the stream.
+    #This is a basic listener that just prints received tweets to standard output
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+#search
+api = tweepy.API(auth)
+twitterStream = Stream(auth,TweetListener())
+desc = []
+
+out = open("no_bots4.csv", 'w')
+
+
+mashape_key = "iykKYyk7XTmshsUERrKagp0XxruZp1mWfoEjsnS24G5TOEopR1"
+twitter_app_auth = {
+    'consumer_key': CONSUMER_KEY,
+    'consumer_secret': CONSUMER_SECRET,
+    'access_token': ACCESS_KEY,
+    'access_token_secret': ACCESS_SECRET,
+  }
+
+bom = botometer.Botometer(mashape_key=mashape_key, **twitter_app_auth)
+
+out.write("username\n")
+
+for user in ids11:
+    out.write(user + "\n")
+
+i = 0
+for user in ids[3000:4000]:
+    i = i + 1
+    try:
+        result = bom.check_account(user)
+        #print(result['scores']['universal'])
+        print(i)
+
+        if result['scores']['universal'] < 0.5:
+            out.write(user + "\n")
+
+    except Exception, e:
+        print(user)
+    
+out.close()
+
+# for i in ids:
+#     ids_s.append(i.partition(' ')[0])
