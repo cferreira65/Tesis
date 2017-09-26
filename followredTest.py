d94479b73518012c6cde08179b0e6a7a152cc445
Dayana Rodrigues <dayana2301@gmail.com>
2017-09-26 16:20:51 -0400

    followred 1000-1500

--- followredTest.py
+++ followredTest.py
@@ -11,36 +11,61 @@ from tweepy import OAuthHandler
 import botometer
 import networkx as nx
 
-df1 = pd.read_csv('test1.csv', sep= ';')
-df2 = pd.read_csv('test2.csv', sep= ';')
+df1 = pd.read_csv('no_bots_1-1000.csv', sep= ';')
+df2 = pd.read_csv('no_bots_1000-2000.csv', sep= ';')
+df3 = pd.read_csv('no_bots_2000-3000.csv', sep= ';')
+df4 = pd.read_csv('no_bots_3000-4000.csv', sep= ';')
+df5 = pd.read_csv('no_bots_4000-5500.csv', sep= ';')
+df6 = pd.read_csv('no_bots_5500-7000.csv', sep= ';')
+df7 = pd.read_csv('no_bots_7000-8500.csv', sep= ';')
+df8 = pd.read_csv('no_bots_8500-10000.csv', sep= ';')
+df9 = pd.read_csv('no_bots_10000-11500.csv', sep= ';')
+df10 = pd.read_csv('no_bots_11500-13000.csv', sep= ';')
+df11 = pd.read_csv('no_bots_13000-14200.csv', sep= ';')
+df12 = pd.read_csv('no_bots_14200-15335.csv', sep= ';')
+df13 = pd.read_csv('Tweets/selected_users_opos.csv', sep =';')
 
-# ids = df1.username #you can also use df['column_name']
-# ids2 = df2.username
-ids_opos = df1.username 
-#ids4 = df4.username
-# ids5 = df5.username
-#ids6 = df6.username
-#ids7 = df7.username
-#ids8 = df8.username
-# ids9 = df9.username
-# ids10 = df10.username
-#ids11 = df11.username
-#ids12 = df12.username
-#ids13 = df13.username
-ids_chav = df2.username
+df14 = pd.read_csv('no_bots_15335-16835.csv', sep= ';')
+df15 = pd.read_csv('no_bots_16835-18335.csv', sep= ';')
+df16 = pd.read_csv('no_bots_18335-19804.csv', sep= ';')
+df17 = pd.read_csv('Tweets/selected_users_chav.csv', sep =';')
 
-# ids = ids.append(ids2, ignore_index=True)
-# ids = ids.append(ids3, ignore_index=True)
-#ids_opos = ids_opos.append(ids4, ignore_index=True)
-# ids = ids.append(ids5, ignore_index=True)
-#ids_opos = ids_opos.append(ids6, ignore_index=True)
-#ids_opos = ids_opos.append(ids7, ignore_index=True)
-#ids_opos = ids_opos.append(ids8, ignore_index=True)
-# ids = ids.append(ids9, ignore_index=True)
-# ids = ids.append(ids10, ignore_index=True)
-#ids_opos = ids_opos.append(ids11, ignore_index=True)
-#ids_opos = ids_opos.append(ids12, ignore_index=True)
-#ids_opos = ids_opos.append(ids13, ignore_index=True)
+ids_opos = df1.username #you can also use df['column_name']
+ids2 = df2.username
+ids3 = df3.username 
+ids4 = df4.username
+ids5 = df5.username
+ids6 = df6.username
+ids7 = df7.username
+ids8 = df8.username
+ids9 = df9.username
+ids10 = df10.username
+ids11 = df11.username
+ids12 = df12.username
+ids13 = df13.username
+ids_chav = df14.username
+ids15 = df15.username
+ids16 = df16.username
+ids17 = df17.username
+
+
+ids_opos = ids_opos.append(ids2)
+ids_opos = ids_opos.append(ids3)
+ids_opos = ids_opos.append(ids4)
+ids_opos = ids_opos.append(ids5)
+ids_opos = ids_opos.append(ids6)
+ids_opos = ids_opos.append(ids7)
+ids_opos = ids_opos.append(ids8)
+ids_opos = ids_opos.append(ids9)
+ids_opos = ids_opos.append(ids10)
+ids_opos = ids_opos.append(ids11)
+ids_opos = ids_opos.append(ids12)
+ids_opos = ids_opos.append(ids13)
+
+ids_chav = ids_chav.append(ids15)
+ids_chav = ids_chav.append(ids16)
+ids_chav = ids_chav.append(ids17)
+
 
 ids_opos = pd.Series(ids_opos.unique())
 ids_chav = pd.Series(ids_chav.unique())
@@ -48,10 +73,10 @@ ids_chav = pd.Series(ids_chav.unique())
 print(ids_opos.size)
 print(ids_chav.size)
 
-CONSUMER_KEY = '5Rcxy0B6hTefj4WfI83Ov4rGn'
-CONSUMER_SECRET = 'IROZKaE6Osnt7FlvVmZlWLEU9V1KT7TyZpda7CgrJKG5Qmtre5'
-ACCESS_KEY = '86460420-9xJaN64nnrumh3QRJEfKWhTFcjf572kOtHGbRMkta'
-ACCESS_SECRET = 'Rarw3wksqYiVDZsTMPebWDztDSuQuXSiIwfz40jgMkrsC'
+CONSUMER_KEY = 'FUjJNyet2iQ3DrmSs8zdclFgG'
+CONSUMER_SECRET = '1l8uippLO9oeJS1b28aPwLqAizI6MkacUXofje6XMcEMEeVAwn'
+ACCESS_KEY = '86460420-zaHoxJV9XSfxnppUTMlmMCtYDgFopK1yg4fiVGBet'
+ACCESS_SECRET = '0uD03WJIlejK5K7YemoSMpXJe4ujII0RfJuERMoiEDLWu'
 auth = OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
 auth.secure = True
 api = tweepy.API(auth)
@@ -79,7 +104,7 @@ g_opos = nx.DiGraph()
 g_chav = nx.DiGraph()
 
 i = 1
-for user in ids_opos[0:10]:
+for user in ids_opos:
     try:
         user_a = api.get_user(screen_name = user)
         desc_opos.append(user_a)
@@ -91,7 +116,7 @@ for user in ids_opos[0:10]:
         print(user)
 
 i = 1
-for user in ids_chav[0:10]:
+for user in ids_chav:
     try:
         user_a = api.get_user(screen_name = user)
         desc_chav.append(user_a)
@@ -104,7 +129,7 @@ for user in ids_chav[0:10]:
 
 i = 0
 # usuarios de los hashtags opositores
-for user in ids_opos[0:10]:
+for user in ids_opos[1000:1500]:
 #     #print user
     i = i + 1
     try:
@@ -128,12 +153,12 @@ for user in ids_opos[0:10]:
                         g_all.add_edge(user2.screen_name,user)
                 except Exception, e:
                     print ("user2 ", user2)
-        print g_all.edges()
+        # print g_all.edges()
         print i
     except Exception, e:
         print(user)
 
-for user in ids_chav[0:10]:
+for user in ids_chav[0:0]:
 #     #print user
     i = i + 1
     try:
@@ -157,12 +182,12 @@ for user in ids_chav[0:10]:
                         g_all.add_edge(user2.screen_name,user)
                 except Exception, e:
                     print ("user2 ", user2)
-        print g_all.edges()
+        # print g_all.edges()
         print i
     except Exception, e:
         print(user)
 
-nx.write_graphml(g_all,'follow_all.xml')
-nx.write_graphml(g_opos,'follow_opos.xml')
-nx.write_graphml(g_chav,'follow_chav.xml')
+nx.write_graphml(g_all,'follow_all_1000-1500.xml')
+nx.write_graphml(g_opos,'follow_opos_1000-1500.xml')
+nx.write_graphml(g_chav,'follow_chav_1000-1500.xml')
 
