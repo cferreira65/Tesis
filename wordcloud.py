0f52c5d36916733b5163f5565ec76d58b0da17e4
Karelys Medina <cferreira6594@gmail.com>
2018-01-12 15:09:38 -0500

    Agregados unos wordclouds

---
+++ wordcloud.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import string
+import pandas as pd
+from nltk.corpus import stopwords
+
+
+#df1 = pd.read_csv('oposTimelineText.csv', sep= ';')
+#print(df1.iloc[0])
+
+data = []
+fp = open('oposTimelineText.csv', 'r')
+line = fp.readline()
+out = open("opos_wordcloud.txt", 'w')
+
+while line:
+    #data.append(line.split(';'))
+    #line = fp.readline()
+
+    data = line.split(';')
+    # stop words
+    train_data = ""
+    for d in data[1:-1]:
+        filtered_words = list(filter(lambda word: word not in stopwords.words('spanish'), d.split()))
+        filtered_words = list(filter(lambda word: word not in stopwords.words('english'), filtered_words))
+        train_data = ' '.join(word for word in filtered_words)
+        out.write(train_data)
+        
+    out.write("\n")
+    line = fp.readline()
+
+
+fp.close
+out.close
+
+data = []
+fp2 = open('chavTimelineText.csv', 'r')
+line = fp2.readline()
+out2 = open("chav_wordcloud.txt", 'w')
+
+while line:
+    #data.append(line.split(';'))
+    #line = fp.readline()
+
+    data = line.split(';')
+    # stop words
+    train_data = ""
+    for d in data[1:-1]:
+        filtered_words = list(filter(lambda word: word not in stopwords.words('spanish'), d.split()))
+        filtered_words = list(filter(lambda word: word not in stopwords.words('english'), filtered_words))
+        train_data = ' '.join(word for word in filtered_words)
+        out2.write(train_data)
+
+    out2.write("\n")
+    line = fp2.readline()
+
+
+fp2.close
+out2.close
