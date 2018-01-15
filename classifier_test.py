5b82de9bb5b2a8905f49158e273414efba0f7cc9
Karelys Medina <cferreira6594@gmail.com>
2018-01-15 17:51:03 -0500

    Agregado el resto de los clasificadores menos los binarios y cambiado todos los sting a minusculas

--- classifier_test.py
+++ classifier_test.py
@@ -74,9 +74,7 @@ i = 0
 
 for data in user1[:500]:
     for d in data:
-        filtered_words = list(filter(lambda word: word not in stopwords.words('spanish'), d.split()))
-        filtered_words = list(filter(lambda word: word not in stopwords.words('english'), filtered_words))
-        train_data.append(' '.join(word for word in filtered_words))
+        train_data.append(d)
         target.append(clas1[i])
     
     i = i +1
@@ -84,51 +82,132 @@ for data in user1[:500]:
 j = 0
 for data in user2[:500]:
     for d in data:
-        filtered_words = list(filter(lambda word: word not in stopwords.words('spanish'), d.split()))
-        filtered_words = list(filter(lambda word: word not in stopwords.words('english'), filtered_words))
-        train_data.append(' '.join(word for word in filtered_words))
+        train_data.append(d)
         target.append(clas2[j])
     
     j = j +1
 
-count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(train_data)
-print(X_train_counts.shape)
-print(count_vect.vocabulary_.get(u'búsqueda'))
+count_vect1 = CountVectorizer()
+count_vect2 = CountVectorizer(ngram_range = (1,2))
 
-tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+X_train_counts1 = count_vect1.fit_transform(train_data)
+X_train_counts2 = count_vect2.fit_transform(train_data)
 
-clf = MultinomialNB().fit(X_train_tfidf, target)
-clf2 = SGDClassifier(loss='hinge', penalty='l2',
+#print(X_train_counts.shape)
+#print(count_vect.vocabulary_.get(u'búsqueda'))
+
+tfidf_transformer1 = TfidfTransformer()
+tfidf_transformer2 = TfidfTransformer()
+
+X_train_tfidf1 = tfidf_transformer1.fit_transform(X_train_counts1)
+X_train_tfidf2 = tfidf_transformer2.fit_transform(X_train_counts2)
+
+# Clasificadores naive bayes
+NB1 = MultinomialNB().fit(X_train_counts1, target)
+NB2 = MultinomialNB().fit(X_train_counts2, target)
+NB3 = MultinomialNB().fit(X_train_tfidf1, target)
+NB4 = MultinomialNB().fit(X_train_tfidf2, target)
+
+
+SGD1 = SGDClassifier(loss='hinge', penalty='l2',
                     alpha=1e-3, random_state=42,
-                    max_iter=5, tol=None).fit(X_train_tfidf, target)
+                    max_iter=5, tol=None).fit(X_train_counts1, target)
+SGD2 = SGDClassifier(loss='hinge', penalty='l2',
+                    alpha=1e-3, random_state=42,
+                    max_iter=5, tol=None).fit(X_train_counts2, target)
+SGD3 = SGDClassifier(loss='hinge', penalty='l2',
+                    alpha=1e-3, random_state=42,
+                    max_iter=5, tol=None).fit(X_train_tfidf1, target)
+SGD4 = SGDClassifier(loss='hinge', penalty='l2',
+                    alpha=1e-3, random_state=42,
+                    max_iter=5, tol=None).fit(X_train_tfidf2, target)
+SGD5 = SGDClassifier(loss='hinge', penalty='l2',
+                    alpha=1e-2, random_state=42,
+                    max_iter=5, tol=None).fit(X_train_counts1, target)
+SGD6 = SGDClassifier(loss='hinge', penalty='l2',
+                    alpha=1e-2, random_state=42,
+                    max_iter=5, tol=None).fit(X_train_counts2, target)
+SGD7 = SGDClassifier(loss='hinge', penalty='l2',
+                    alpha=1e-2, random_state=42,
+                    max_iter=5, tol=None).fit(X_train_tfidf1, target)
+SGD8 = SGDClassifier(loss='hinge', penalty='l2',
+                    alpha=1e-2, random_state=42,
+                    max_iter=5, tol=None).fit(X_train_tfidf2, target)
 
-NBprediction = []
-SGDprediction = []
+
+NB1prediction = []
+NB2prediction = []
+NB3prediction = []
+NB4prediction = []
+
+SGD1prediction = []
+SGD2prediction = []
+SGD3prediction = []
+SGD4prediction = []
+SGD5prediction = []
+SGD6prediction = []
+SGD7prediction = []
+SGD8prediction = []
+
 test_clasification = []
 
 for data in user1[500:]:
     test_data = []
 
     for d in data:
-        # print(i.split())
-        filtered_words = list(filter(lambda word: word not in stopwords.words('spanish'), d.split()))
-        filtered_words = list(filter(lambda word: word not in stopwords.words('english'), filtered_words))
-        test_data.append(' '.join(word for word in filtered_words))
+        test_data.append(d)
 
     # print(test_data)
-    X_new_counts = count_vect.transform(test_data)        
-    X_new_tfidf = tfidf_transformer.transform(X_new_counts)
-    predictedNB = clf.predict(X_new_tfidf)
+    X_new_counts1 = count_vect1.transform(test_data)
+    X_new_counts2 = count_vect2.transform(test_data)        
+
+    X_new_tfidf1 = tfidf_transformer1.transform(X_new_counts1)
+    X_new_tfidf2 = tfidf_transformer2.transform(X_new_counts2)
+
+    predictedNB1 = NB1.predict(X_new_counts1)
+    predictedNB2 = NB2.predict(X_new_counts2)
+
+    predictedNB3 = NB3.predict(X_new_tfidf1)
+    predictedNB4 = NB4.predict(X_new_tfidf2)
+
     # predicted = text_clf.predict(test_data)
-    predictedSGD = clf2.predict(X_new_tfidf)
-    res = result(predictedNB)
-    NBprediction.append(res)
-    print("La predicción de NB es: " + str(res))
-    res = result(predictedSGD)
-    SGDprediction.append(res)        
-    print("La predicción de SGD es: " + str(res))
+    predictedSGD1 = SGD1.predict(X_new_counts1)
+    predictedSGD2 = SGD2.predict(X_new_counts2)
+    predictedSGD3 = SGD3.predict(X_new_tfidf1)
+    predictedSGD4 = SGD4.predict(X_new_tfidf2)
+    predictedSGD5 = SGD5.predict(X_new_counts1)
+    predictedSGD6 = SGD6.predict(X_new_counts2)
+    predictedSGD7 = SGD7.predict(X_new_tfidf1)
+    predictedSGD8 = SGD8.predict(X_new_tfidf2)
+
+
+    res = result(predictedNB1)
+    NB1prediction.append(res)
+    res = result(predictedNB2)
+    NB2prediction.append(res)
+    res = result(predictedNB3)
+    NB3prediction.append(res)
+    res = result(predictedNB4)
+    NB4prediction.append(res)
+    #print("La predicción de NB es: " + str(res))
+    
+    res = result(predictedSGD1)
+    SGD1prediction.append(res)
+    res = result(predictedSGD2)
+    SGD2prediction.append(res)  
+    res = result(predictedSGD3)
+    SGD3prediction.append(res)  
+    res = result(predictedSGD4)
+    SGD4prediction.append(res)  
+    res = result(predictedSGD5)
+    SGD5prediction.append(res)  
+    res = result(predictedSGD6)
+    SGD6prediction.append(res)  
+    res = result(predictedSGD7)
+    SGD7prediction.append(res)  
+    res = result(predictedSGD8)
+    SGD8prediction.append(res)          
+    #print("La predicción de SGD es: " + str(res))
     test_clasification.append(clas1[i])
     i = i + 1
 
@@ -136,45 +215,155 @@ for data in user2[500:]:
     test_data = []
 
     for d in data:
-        # print(i.split())
-        filtered_words = list(filter(lambda word: word not in stopwords.words('spanish'), d.split()))
-        filtered_words = list(filter(lambda word: word not in stopwords.words('english'), filtered_words))
-        test_data.append(' '.join(word for word in filtered_words))
+        test_data.append(d)
 
     # print(test_data)
-    X_new_counts = count_vect.transform(test_data)        
-    X_new_tfidf = tfidf_transformer.transform(X_new_counts)
-    predictedNB = clf.predict(X_new_tfidf)
+    X_new_counts1 = count_vect1.transform(test_data)
+    X_new_counts2 = count_vect2.transform(test_data)        
+
+    X_new_tfidf1 = tfidf_transformer1.transform(X_new_counts1)
+    X_new_tfidf2 = tfidf_transformer2.transform(X_new_counts2)
+
+    predictedNB1 = NB1.predict(X_new_counts1)
+    predictedNB2 = NB2.predict(X_new_counts2)
+
+    predictedNB3 = NB3.predict(X_new_tfidf1)
+    predictedNB4 = NB4.predict(X_new_tfidf2)
+
     # predicted = text_clf.predict(test_data)
-    predictedSGD = clf2.predict(X_new_tfidf)
-    res = result(predictedNB)
-    NBprediction.append(res)
-    print("La predicción de NB es: " + str(res))
-    res = result(predictedSGD)
-    SGDprediction.append(res)        
-    print("La predicción de SGD es: " + str(res))
+    predictedSGD1 = SGD1.predict(X_new_counts1)
+    predictedSGD2 = SGD2.predict(X_new_counts2)
+    predictedSGD3 = SGD3.predict(X_new_tfidf1)
+    predictedSGD4 = SGD4.predict(X_new_tfidf2)
+    predictedSGD5 = SGD5.predict(X_new_counts1)
+    predictedSGD6 = SGD6.predict(X_new_counts2)
+    predictedSGD7 = SGD7.predict(X_new_tfidf1)
+    predictedSGD8 = SGD8.predict(X_new_tfidf2)
+
+
+    res = result(predictedNB1)
+    NB1prediction.append(res)
+    res = result(predictedNB2)
+    NB2prediction.append(res)
+    res = result(predictedNB3)
+    NB3prediction.append(res)
+    res = result(predictedNB4)
+    NB4prediction.append(res)
+    #print("La predicción de NB es: " + str(res))
+    
+    res = result(predictedSGD1)
+    SGD1prediction.append(res)
+    res = result(predictedSGD2)
+    SGD2prediction.append(res)  
+    res = result(predictedSGD3)
+    SGD3prediction.append(res)  
+    res = result(predictedSGD4)
+    SGD4prediction.append(res)  
+    res = result(predictedSGD5)
+    SGD5prediction.append(res)  
+    res = result(predictedSGD6)
+    SGD6prediction.append(res)  
+    res = result(predictedSGD7)
+    SGD7prediction.append(res)  
+    res = result(predictedSGD8)
+    SGD8prediction.append(res)          
+    #print("La predicción de SGD es: " + str(res))
     test_clasification.append(clas2[j])
     j = j + 1 
 
+
+#fp1 = open('resultNB.txt', 'a')
+#fp2 = open('resultSVM.txt', 'a')
+
 # print(train_data)
-print(predictedNB)
-print(predictedSGD)
+print(predictedNB1)
+print(predictedSGD1)
 
 # print(predicted)
 
 print(len(train_data))
 print(len(target))
 
-print("Estadisticas de NB:")
+print("Estadisticas de NB1:")
 
-print(metrics.classification_report(test_clasification, NBprediction,
+print(metrics.classification_report(test_clasification, NB1prediction,
         target_names=["Chavista","Opositor","Ninguno"]))
 
-print(metrics.confusion_matrix(test_clasification, NBprediction))
+print(metrics.confusion_matrix(test_clasification, NB1prediction))
 
-print("Estadisticas de SVM:")
+print("Estadisticas de NB2:")
 
-print(metrics.classification_report(test_clasification, SGDprediction,
+print(metrics.classification_report(test_clasification, NB2prediction,
         target_names=["Chavista","Opositor","Ninguno"]))
 
-print(metrics.confusion_matrix(test_clasification, SGDprediction))
+print(metrics.confusion_matrix(test_clasification, NB2prediction))
+
+print("Estadisticas de NB3:")
+
+print(metrics.classification_report(test_clasification, NB3prediction,
+        target_names=["Chavista","Opositor","Ninguno"]))
+
+print(metrics.confusion_matrix(test_clasification, NB3prediction))
+
+print("Estadisticas de NB4:")
+
+print(metrics.classification_report(test_clasification, NB4prediction,
+        target_names=["Chavista","Opositor","Ninguno"]))
+
+print(metrics.confusion_matrix(test_clasification, NB4prediction))
+
+print("Estadisticas de SVM1:")
+
+print(metrics.classification_report(test_clasification, SGD1prediction,
+        target_names=["Chavista","Opositor","Ninguno"]))
+
+print(metrics.confusion_matrix(test_clasification, SGD1prediction))
+
+print("Estadisticas de SVM2:")
+
+print(metrics.classification_report(test_clasification, SGD2prediction,
+        target_names=["Chavista","Opositor","Ninguno"]))
+
+print(metrics.confusion_matrix(test_clasification, SGD2prediction))
+
+print("Estadisticas de SVM3:")
+
+print(metrics.classification_report(test_clasification, SGD3prediction,
+        target_names=["Chavista","Opositor","Ninguno"]))
+
+print(metrics.confusion_matrix(test_clasification, SGD3prediction))
+
+print("Estadisticas de SVM4:")
+
+print(metrics.classification_report(test_clasification, SGD4prediction,
+        target_names=["Chavista","Opositor","Ninguno"]))
+
+print(metrics.confusion_matrix(test_clasification, SGD4prediction))
+
+print("Estadisticas de SVM5:")
+
+print(metrics.classification_report(test_clasification, SGD5prediction,
+        target_names=["Chavista","Opositor","Ninguno"]))
+
+print(metrics.confusion_matrix(test_clasification, SGD5prediction))
+
+print("Estadisticas de SVM6:")
+
+print(metrics.classification_report(test_clasification, SGD6prediction,
+        target_names=["Chavista","Opositor","Ninguno"]))
+
+print(metrics.confusion_matrix(test_clasification, SGD6prediction))
+
+print("Estadisticas de SVM7:")
+
+print(metrics.classification_report(test_clasification, SGD7prediction,
+        target_names=["Chavista","Opositor","Ninguno"]))
+
+print(metrics.confusion_matrix(test_clasification, SGD7prediction))
+
+print("Estadisticas de SVM8:")
+
+print(metrics.classification_report(test_clasification, SGD8prediction,
+        target_names=["Chavista","Opositor","Ninguno"]))
+
+print(metrics.confusion_matrix(test_clasification, SGD8prediction))
--- stopwords.py
+++ stopwords.py
@@ -12,7 +12,7 @@ from nltk.corpus import stopwords
 data = []
 fp = open('oposTimelineText.csv', 'r')
 line = fp.readline()
-out = open("opos_wordcloud.txt", 'w')
+out = open("opos_stopwords.txt", 'w')
 
 while line:
     #data.append(line.split(';'))
@@ -21,14 +21,17 @@ while line:
     data = line.split(';')
     # stop words
     train_data = ""
+    out.write(data[0])
+    out.write(";")
     for d in data[1:-1]:
         d = d.lower()
         filtered_words = list(filter(lambda word: word not in stopwords.words('spanish'), d.split()))
         filtered_words = list(filter(lambda word: word not in stopwords.words('english'), filtered_words))
         train_data = ' '.join(word for word in filtered_words)
         out.write(train_data)
-        
-    out.write("\n")
+        out.write(";")
+    
+    out.write(data[-1])     
     line = fp.readline()
 
 
@@ -38,7 +41,7 @@ out.close
 data = []
 fp2 = open('chavTimelineText.csv', 'r')
 line = fp2.readline()
-out2 = open("chav_wordcloud.txt", 'w')
+out2 = open("chav_stopwords.txt", 'w')
 
 while line:
     #data.append(line.split(';'))
@@ -47,14 +50,17 @@ while line:
     data = line.split(';')
     # stop words
     train_data = ""
+    out2.write(data[0])
+    out2.write(";")
     for d in data[1:-1]:
         d = d.lower()
         filtered_words = list(filter(lambda word: word not in stopwords.words('spanish'), d.split()))
         filtered_words = list(filter(lambda word: word not in stopwords.words('english'), filtered_words))
         train_data = ' '.join(word for word in filtered_words)
         out2.write(train_data)
+        out2.write(";")
 
-    out2.write("\n")
+    out2.write(data[-1])
     line = fp2.readline()
 
 
