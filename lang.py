6a5b8a4590f6ab08d6cd45f4082405282d4bd60e
Dayana Rodrigues <dayana2301@gmail.com>
2018-06-03 21:43:19 -0400

    language detection and stemming done

--- lang.py
+++ lang.py
@@ -3,6 +3,13 @@ import Stemmer
 
 # DetectorFactory.seed = 0
 
+def quit_character(character, text):
+    new_text = ""
+    for i in text.replace('\n','').encode('utf-8'):
+        new_text =  new_text + i.strip(character)
+
+    return new_text 
+
 def robust_decode(bs):
     '''Takes a byte string as param and convert it into a unicode one.
 First tries UTF8, and fallback to Latin1 if it fails'''
@@ -29,56 +36,92 @@ def get_language(sentence):
 
 data = []
 fp = open('stopwords/opos_stopwords.txt', 'r')
+out1 = open("timelines/oposTimelineStemming.csv", 'w')
 line = fp.readline()
-# fp2 = open('stopwords/chav_stopwords.txt', 'r')
+fp2 = open('stopwords/chav_stopwords.txt', 'r')
+out2 = open("timelines/chavTimelineStemming.csv", 'w')
+opos = []
+chav = []
 
-# while line:
-#     #data.append(line.split(';'))
-#     #line = fp.readline()
+while line:
 
-data = line.split(';')
-user = data.pop(0)
-result = []
+	data = line.split(';')
+	user = [data.pop(0)]
+	result = []
 
-new_text = ""
-for twt in data: 
-	stemmer = Stemmer.Stemmer(get_language(robust_decode(twt)))
-	for word in twt.split():
-		new_text = new_text + stemmer.stemWord(word) + " "
-	result.append(new_text)
+	new_text = ""
+	for twt in data:
+		try: 
+			twt = robust_decode(twt)
+			stemmer = Stemmer.Stemmer(get_language(twt))
+			for word in twt.split():
+				new_text = new_text + stemmer.stemWord(word) + " "
+			print(new_text)
+			result.append(new_text)
+		except:
+			result.append(twt)
 
-print(result)
+	opos.append(user.append(result))
+	line = fp.readline()
 
+i = 0
+for user in opos:
+    try:
+        for tw in user:
+            out1.write(quit_character(';', tw.text))
+            out1.write(";")
+		
+		
+    except Exception, e:
+        print(user)
+        i = i + 1
+        print(e)
 
-    # user.append(data[1:-1])
-    # if target_dictionary[data[-1]] == 2:
-    #     clas.append(1)
-    # else:
-    #     clas.append(target_dictionary[data[-1]])
-    #line = fp.readline()
-
-# line = fp2.readline()
-
-# while line:
-#     #data.append(line.split(';'))
-#     #line = fp.readline()
-
-#     data = line.split(';')
-#     user.append(data[1:-1])
-#     if target_dictionary[data[-1]] == 2:
-#         clas.append(0)
-#     else:
-#         clas.append(target_dictionary[data[-1]])
-#     line = fp2.readline()
+	out1.write("\n")
+        i = i + 1
+	
 
 fp.close
-#fp2.close
+out1.close()
+
+line = fp2.readline()
+
+while line:
+
+	data = line.split(';')
+	user = [data.pop(0)]
+	result = []
+
+	new_text = ""
+	for twt in data:
+		try: 
+			twt = robust_decode(twt)
+			stemmer = Stemmer.Stemmer(get_language(twt))
+			for word in twt.split():
+				new_text = new_text + stemmer.stemWord(word) + " "
+			print(new_text)
+			result.append(new_text)
+		except:
+			result.append(twt)
+
+	chav.append(user.append(result))
+	line = fp2.readline()
 
 
-# new_text = ""
-# for word in text.split():
-# 	new_text = new_text + stemmer.stemWord(word) + " "
+i = 0
+for user in chav:
+    try:
+        for tw in timeline:
+            out2.write(quit_character(';', tw.text))
+            out2.write(";")
 
+    except Exception, e:
+        print(user)
+        i = i + 1
+        print(e)
 
-# print(new_text)
-
+	out2.write("\n")
+    i = i + 1
+	
+out2.close()
+fp2.close
