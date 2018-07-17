83ff1a859e4e5b8bbbe1ead5250888e36d430f59
Dayana Rodirgues <dayana2301@gmail.com>
2018-07-17 13:02:34 -0400

    demo simple

--- demo.py
+++ demo.py
@@ -107,13 +107,7 @@ def clean_demo_stopwords():
 	out.close
 
 
-# CONJUNTO DE ENTRENAMIENTO
-
-def train_data():
-
-	random.seed(42)
-
-	def result(lis):
+def result(lis):
 		count = [0,0]
 
 		for prediction in lis:
@@ -129,148 +123,135 @@ def train_data():
 			res = random.randint(0,1)
 		return res
 
+# CONJUNTO DE ENTRENAMIENTO
+
+def train():
+
+	random.seed(42)
+
 	target_dictionary = {'Chavista\n': 0, 'Oposición\n': 1, 'Ninguno\n':2}
 
 	target = array('i')
-	data = []
 	fp = open('stopwords/opos_stopwords.txt', 'r')
 	line = fp.readline()
 	fp2 = open('stopwords/chav_stopwords.txt', 'r')
 	fp3 = open('demo/stopwords.txt', 'r')
 
-
-	user = []
-	text_user = []
-	clas = []
-	clas_user = []
-	train_data = []
+	print("A")
 
 	while line:
-		#data.append(line.split(';'))
-		#line = fp.readline()
 
 		data = line.split(';')
-		user.append(data[1:-1])
 		if target_dictionary[data[-1]] != 2:
+			user.append(data[1:-1])
 			clas.append(target_dictionary[data[-1]])
 		line = fp.readline()
 
 	line = fp2.readline()
 
+	print("B")
+
 	while line:
-		#data.append(line.split(';'))
-		#line = fp.readline()
 
 		data = line.split(';')
-		user.append(data[1:-1])
 		if target_dictionary[data[-1]] != 2:
+			user.append(data[1:-1])
 			clas.append(target_dictionary[data[-1]])
 		line = fp2.readline()
 
-	fp.close
-	fp2.close
+	
+	print("C")
 
 	line = fp3.readline()
-	while line:
+	data = line.split(';')
+	text_user.append(data[1:-1])
 
-		data = line.split(';')
-		text_user.append(data[1:-1])
-		# if target_dictionary[data[-1]] != 2:
-		# 	clas_user.append(target_dictionary[data[-1]])
-		# line = fp3.readline()
+	fp.close
+	fp2.close
+	fp3.close
+
+	i = 0
+	target = array('i')
+	print("D")
+	# print(clas)
+	for t in user:
+		for d in user[i]:
+			train_data.append(d)
+			target.append(clas[i])
+		i = i + 1
 
 
 	statistic = [[], [], []]
 
-	kf = StratifiedKFold(n_splits=10, random_state=42, shuffle = True)
+	#count_vect= CountVectorizer(ngram_range = (1,2))
 
-	i = 1
-	for train, test in kf.split(user,clas):
-		print train
-		print test
-		train_data = []
-		target = array('i')
-		for t in train:
-			for d in user[t]:
-				train_data.append(d)
-				target.append(clas[t])
+	X_train_counts = count_vect.fit_transform(train_data)
+	
 
-		count_vect= CountVectorizer(ngram_range = (1,2))
+	X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
-		X_train_counts = count_vect.fit_transform(train_data)
-		
+	print("E")
 
-		#print(X_train_counts.shape)
-		#print(count_vect.vocabulary_.get(u'búsqueda'))
-
-		tfidf_transformer = TfidfTransformer()
-
-		X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-
-		#  n-gram=1 y 2, tf-idf=true, C=0.5
-
-		SGD1 = SGDClassifier(loss='hinge', penalty='l2',
+	SGD = SGDClassifier(loss='hinge', penalty='l2',
 							alpha=1e-3, random_state=42,
 							max_iter=5, tol=None).fit(X_train_counts, target)
-		
-		#SGD5 = SGDClassifier(loss='hinge', penalty='l2',
-		 #                   alpha=1e-2, random_state=42,
-		  #                  max_iter=5, tol=None).fit(X_train_counts, target)
+
+	return SGD	
 		
 
-		prediction = []
+def predict(trained):
 
-		test_clasification = []
+	prediction = []
 
-		for t in user_test:
-			test_data = []
-			for d in user_user[t]:
-				test_data.append(d)
+	test_clasification = []
 
-			X_new_counts = count_vect.transform(test_data)        
-
-			X_new_tfidf= tfidf_transformer.transform(X_new_counts)
-
-			predicted = SDG1.predict(X_new_tfidf)
-
-
-
-			res = result(predicted)
-			prediction.append(res)   
-
-			test_clasification.append(user_clas[t])
-
-		string = "demo/resultados.txt"
-		out = open(string, 'w')
-
-
-		out.write("Estadisticas:\n")
-
-		out.write(str(metrics.classification_report(test_clasification, prediction,
-					target_names=["Chavista","Opositor"])))
-
-		out.write(str(metrics.confusion_matrix(test_clasification, prediction)))
-
-		out.write("\n")
-
-		statistic[0].append(res[0])
-		statistic[1].append(res[1])
-		statistic[2].append(res[2])
-
-	out = open("demo/resultados", 'w')
-
-	out.write("Promedio:\n")
-	out.write("Precision: " + str(np.mean(statistic[0])) + "\n")
-	out.write("Recall: " + str(np.mean(statistic[1])) + "\n")
-	out.write("Fscore: " + str(np.mean(statistic[2])) + "\n")
-
-
+	test_data = []
 
 	
-print("Please enter the Twitter username:")
-username = raw_input()
+	for d in text_user[0]:
+
+		test_data.append(d)
+
+		X_new_counts = count_vect.transform(test_data)        
+
+		X_new_tfidf= tfidf_transformer.transform(X_new_counts)
+
+		predicted = trained.predict(X_new_tfidf)
+
+
+		res = result(predicted)
+		prediction.append(res)   
+
+	
+	print(prediction)
+
+	# string = "demo/resultados.txt"
+	# out = open(string, 'w')
+
+
+	# out.write("Estadisticas:\n")
+
+	# out.write(str(metrics.classification_report(test_clasification, prediction,
+	# 				target_names=["Chavista","Opositor"])))
+
+	# out.write(str(metrics.confusion_matrix(test_clasification, prediction)))
+
+	# out.write("\n")
+
+	# statistic[0].append(res[0])
+	# statistic[1].append(res[1])
+	# statistic[2].append(res[2])
+
+	# out = open("demo/resultados", 'w')
+
+	# out.write("Promedio:\n")
+	# out.write("Precision: " + str(np.mean(statistic[0])) + "\n")
+	# out.write("Recall: " + str(np.mean(statistic[1])) + "\n")
+	# ut.write("Fscore: " + str(np.mean(statistic[2])) + "\n")
+
 
 # OBTENCION DE DATOS TWITTER
+
 CONSUMER_KEY = 'FUjJNyet2iQ3DrmSs8zdclFgG'
 CONSUMER_SECRET = '1l8uippLO9oeJS1b28aPwLqAizI6MkacUXofje6XMcEMEeVAwn'
 ACCESS_KEY = '86460420-zaHoxJV9XSfxnppUTMlmMCtYDgFopK1yg4fiVGBet'
@@ -280,9 +261,31 @@ auth.secure = True
 api = tweepy.API(auth)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
-getTimeline(username)
-clean_demo_stopwords()
-train_data()
+data = []
+user = []
+text_user = []
+clas = []
+user_clas = []
+train_data = []
+user_test = []
+
+count_vect= CountVectorizer(ngram_range = (1,2))
+tfidf_transformer = TfidfTransformer()
+print("Training, please wait...\n")
+
+trained = train()
+
+while True:
+	print("Please enter the Twitter username:\n")
+	username = raw_input()
+	print("Searching the user info from Twitter...\n")
+	getTimeline(username)
+	clean_demo_stopwords()
+	print("Prediction:\n")
+	predict(trained)
+
+
+
 
 
 
